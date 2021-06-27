236bf09236c1047b3c58e17ffb8e455b6fdc0799
Axtunmz <67156986+Zertified@users.noreply.github.com>
2021-06-27 15:01:42 +0000

    Updated some parts of the teams data

--- data.py
+++ data.py
@@ -1,6 +1,22 @@
 arsenal = {
-	"overall":"81",
-	"attacking strength":"84",
+	"overall":"80",
+	"attacking strength":"83",
+	"defensive strength": "79",
+	"midfield strength": "79",
+	"home goals": "24",
+	"away goals": "31"
+}
+
+aston_villa = {
+	"overall":"77",
+	"attacking strength":"77",
 	"defensive strength": "77",
+	"midfield strength": "77",
+	"home goals": "29",
+	"away goals": "26"
+}
+
+blackburn_rovers = {
 	
 }
+
